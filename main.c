7622917c103f44eed7941f06c1a7e54291ab7359
lollo <lollogado@gmail.com>
2019-08-22 00:24:48 +0200

    Code Fix

--- main.c
+++ main.c
@@ -51,12 +51,14 @@ struct Couples {  //coppia sorgente destinazione collegata da una relazione
     struct PlainEnt *destination;
 };
 
-//----------------------------------------------------------------------------------------------------------------------
+//-----PROTOTYPES-------------------------------------------------------------------------------------------------------
 
 int hash64(char input);
 
+struct PlainEnt *EntityLookup(char *inputName, unsigned int tableHash, struct EntTable *entHash);
 
-//----------------------------------------------------------------------------------------------------------------------
+
+//-----MEMORY INIT------------------------------------------------------------------------------------------------------
 
 //Questa funzione crea la tabella di hashing per le entità. Gestisce 64*64 indici (hashing numerico per i primi due
 // caratteri) più una colonna speciale in caso di entità con nome composto da 1 carattere.
@@ -89,7 +91,7 @@ struct RelTable *initRelHash() {
 
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//-----COMMANDS---------------------------------------------------------------------------------------------------------
 
 //Questa funzione viene chiamata quando in input leggo il comando addent, ne calcolo l hash, e aggiungo in coda
 //l' entità passata dal comando, sse questa non è gia presente nella tabella.
@@ -99,32 +101,20 @@ void HashInputEnt(struct EntTable *hashTable) {
 
     char *inputEnt = NULL;
     int tableIndex;
-    bool found = false;
+    struct PlainEnt *result = NULL;
+
 
     scanf("%ms", &inputEnt);      //Leggo l' entità
 
     if (strlen(inputEnt) > 3) {
-
         tableIndex = hash64(inputEnt[1]) * 64 + hash64(inputEnt[2]);   //Se ha più di 1 carattere, hashing
-
     } else {
-
         tableIndex = 4096;  //Altrimenti array di singletons
-
     }
 
+    result = EntityLookup(inputEnt, tableIndex, hashTable);
 
-    for (unsigned int a = 0; a < hashTable[tableIndex].entNumber; a++) {
-
-        if (strcmp(hashTable[tableIndex].entEntries[a].entName, inputEnt) == 0) {
-
-            found = true;  //Se l' entità è gia monitorata, non la aggiungo
-            break;
-
-        }
-    }
-
-    if (!found) {
+    if (result == NULL) {
 
         if (hashTable[tableIndex].entEntries == NULL) {
 
@@ -140,14 +130,15 @@ void HashInputEnt(struct EntTable *hashTable) {
         //Aggiungo una casella in cui salvare l' entità e la aggiungo
 
         hashTable[tableIndex].entEntries = realloc(hashTable[tableIndex].entEntries,
-                                                   hashTable[tableIndex].entNumber * sizeof(struct PlainEnt));
+                                                   temp * sizeof(struct PlainEnt));
 
-        hashTable[tableIndex].entEntries[temp - 1].entName = malloc(1); //cerco un indirizzo per la stringa.
+        hashTable[tableIndex].entEntries[temp - 1].entName = malloc(
+                1); //cerco un indirizzo per la stringa. Segfaulta altrimenti
+
         strcpy(hashTable[tableIndex].entEntries[temp - 1].entName, inputEnt);
 
     }
 }
-
 //----------------------------------------------------------------------------------------------------------------------
 
 //Questa funzione viene chiamata quando in input leggo il comando addrel, ne calcolo l hash, e aggiungo la realzione
@@ -163,10 +154,11 @@ void HashInputRel(struct RelTable *relHashTable, struct EntTable *entHashTable) 
     int hashedSrc = 0;
     int hashedDest = 0;
     int tableIndex = 0;
-    bool srcFound = false;
-    bool destFound = false;
-    bool relFound = false;
+    struct PlainEnt *srcFound = NULL;
+    struct PlainEnt *destFound = NULL;
+    struct PlainEnt *relFound = NULL;
 
+    //Leggo i parametri del comando
 
     scanf("%ms", &src);
     scanf("%ms", &dest);
@@ -174,67 +166,30 @@ void HashInputRel(struct RelTable *relHashTable, struct EntTable *entHashTable) 
 
     //Per prima cosa verifico che le due entità esistano nella EntTable
 
-
-    if (strlen(src) > 3) {  //Cerco la sorgente
-
+    if (strlen(src) > 3) {  //Calcolo l' hash
         hashedSrc = hash64(src[1]) * 64 + hash64(src[2]);
-
-        for (unsigned int a = 0; a < entHashTable[hashedSrc].entNumber; a++) {
-
-            if (strcmp(entHashTable[hashedSrc].entEntries[a].entName, src) == 0) {
-
-                srcFound = true;
-                srcPtr = &(entHashTable[hashedSrc].entEntries[a]);
-                break;
-            }
-        }
-
     } else {
-
-        for (unsigned int a = 0; a < entHashTable[4096].entNumber; a++) {
-
-            if (strcmp(entHashTable[4096].entEntries[a].entName, src) == 0) {
-
-                srcFound = true;
-                srcPtr = &(entHashTable[4096].entEntries[a]);
-                break;
-            }
-        }
+        hashedSrc = 4096;
     }
 
-    if (srcFound) { //Se non trovo la sorgente è inutile cercare la dest, tanto poi scarterei il comando
+    srcFound = EntityLookup(src, hashedSrc, entHashTable); //Cerco la sorgente
+
+    if (srcFound != NULL) { //Se non trovo la sorgente è inutile cercare la dest, tanto poi scarterei il comando
 
         if (strlen(dest) > 3) {  //Cerco la destinazione
-
             hashedDest = hash64(dest[1]) * 64 + hash64(dest[2]);
-
-            for (unsigned int a = 0; a < entHashTable[hashedDest].entNumber; a++) {
-
-                if (strcmp(entHashTable[hashedDest].entEntries[a].entName, src) == 0) {
-
-                    destFound = true;
-                    destPtr = &(entHashTable[hashedDest].entEntries[a]);
-                    break;
-                }
-            }
         } else {
-
-            for (unsigned int a = 0; a < entHashTable[4096].entNumber; a++) {
-
-                if (strcmp(entHashTable[4096].entEntries[a].entName, src) == 0) {
-
-                    destFound = true;
-                    destPtr = &(entHashTable[4096].entEntries[a]);
-                    break;
-                }
-            }
+            hashedDest = 4096;
         }
+
+        destFound = EntityLookup(dest, hashedDest, entHashTable);
+
     }
 
 
     //Ho trovato entrambe, proseguo. Se non trovo entrambe non faccio nulla.
 
-    if (srcFound && destFound) {
+    if (srcFound != NULL && destFound != NULL) {
 
         if (strlen(inputRel) > 3) {
 
@@ -297,82 +252,28 @@ void HashInputRel(struct RelTable *relHashTable, struct EntTable *entHashTable) 
 
                     relHashTable[tableIndex].relEntries[a].binded[cplNumbTemp - 1].destination = destPtr;
 
-
                 }
+
 
             }
         }
 
 
     }
+
 }
 //TODO 3
 //----------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
-
-
+void DeleteEnt() {};
 
 //----------------------------------------------------------------------------------------------------------------------
 
-//Questa funzione calcola l indice dell' array in cui inserire l' entità o la relazione. La chiave ritornata è calcolata
-//in base a una regola di riduzione dei caratteri validi in input da un indice base 64 a un indice decimale.
+void DeleteRel() {};
 
-//La compressione dei valori ascii di un carattere a valori compresi fra 0 e 63 avviene come:
+//----------------------------------------------------------------------------------------------------------------------
 
-/* Se incontro il carattere "-", gli assegno il valore 0;
- * Se incontro una cifra sottraggo al suo valore ascii 47;
- * Se incontro una lettera maiuscola sottraggo 54
- * Se incontro il carattere "_" gli assegno il valore 37
- * Se incontro una lettera minuscola sottraggo 59
- */
-
-//Una volta compressa la lettera, calcolo l hash come:
-
-//Val_prima * 64 + val_seconda.
-
-//Questo mi ritorna un indice compreso fra 0 e 4095, e lascia il valore indice 4096 per le entità che son più corte di
-//2 caratteri.
-
-
-inline int hash64(char input) {    //Gioele     71  105
-
-
-    int hashed = 0;
-
-    hashed = input;
-
-    if (input == 45) {  //Carattere '-'
-
-        hashed = 0;
-
-
-    } else if (48 <= input && input <= 57) {  //Cifre 0...9
-
-        hashed = input - 47;
-
-    } else if (65 <= input && input <= 90) { //Lettere Maiuscole A..Z
-
-        hashed = input - 54;
-
-
-    } else if (input == 95) { //Carattere '_'
-
-        hashed = 37;
-
-    } else if (97 <= input && input <= 122) {  //Lettere minuscole a...z
-
-        hashed = input - 59;
-
-    }
-
-    return hashed;
-
-}
-
+void Report();
 
 //----------------------------------------------------------------------------------------------------------------------
 //Questa funzione legge il main txt e si occupa di richiamare le diverse funzioni di parsing nel caso di comando su entità, comando su
@@ -431,6 +332,102 @@ static inline bool ParseTxt(struct EntTable *entTable, struct RelTable *relTable
 
 }
 
+//-----HELPERS----------------------------------------------------------------------------------------------------------
+
+//Questa funzione calcola l indice dell' array in cui inserire l' entità o la relazione. La chiave ritornata è calcolata
+//in base a una regola di riduzione dei caratteri validi in input da un indice base 64 a un indice decimale.
+
+//La compressione dei valori ascii di un carattere a valori compresi fra 0 e 63 avviene come:
+
+/* Se incontro il carattere "-", gli assegno il valore 0;
+ * Se incontro una cifra sottraggo al suo valore ascii 47;
+ * Se incontro una lettera maiuscola sottraggo 54
+ * Se incontro il carattere "_" gli assegno il valore 37
+ * Se incontro una lettera minuscola sottraggo 59
+ */
+
+//Una volta compressa la lettera, calcolo l hash come:
+
+//Val_prima * 64 + val_seconda.
+
+//Questo mi ritorna un indice compreso fra 0 e 4095, e lascia il valore indice 4096 per le entità che son più corte di
+//2 caratteri.
+
+
+inline int hash64(char input) {    //Gioele     71  105
+
+
+    int hashed = 0;
+
+    hashed = input;
+
+    if (input == 45) {  //Carattere '-'
+
+        hashed = 0;
+
+
+    } else if (48 <= input && input <= 57) {  //Cifre 0...9
+
+        hashed = input - 47;
+
+    } else if (65 <= input && input <= 90) { //Lettere Maiuscole A..Z
+
+        hashed = input - 54;
+
+
+    } else if (input == 95) { //Carattere '_'
+
+        hashed = 37;
+
+    } else if (97 <= input && input <= 122) {  //Lettere minuscole a...z
+
+        hashed = input - 59;
+
+    }
+
+    return hashed;
+
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+//Questa funzione scansiona la tabella di entità per verificare se un' entità esiste già in memoria.
+
+struct PlainEnt *EntityLookup(char *inputName, unsigned int tableHash, struct EntTable *entHash) {
+
+    for (unsigned int i = 0; i < entHash[tableHash].entNumber; i++) {
+
+        if (strcmp(entHash[tableHash].entEntries[i].entName, inputName) == 0) {
+
+            return &(entHash[tableHash].entEntries[i]);
+
+        }
+    }
+
+    return NULL;
+}
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+struct PlainRel *RelationLookup(char *inputName, unsigned int tableHash, struct RelTable *relHash) {
+
+    for (unsigned int i = 0; i < relHash[tableHash].relNumber; i++) {
+
+        if (strcmp(relHash[tableHash].relEntries[i].relName, inputName) == 0) {
+
+            return &(relHash[tableHash].relEntries[i]);
+
+        }
+    }
+
+    return NULL;
+}
+
+
+
+//-----MAIN-------------------------------------------------------------------------------------------------------------
+
 
 int main() {
 
@@ -447,6 +444,8 @@ int main() {
  *        è contenuta l' entità monitorata. Diventa pericoloso in termini di spazio, ma sicuramente non dover fare continue scansioni della
  *        hash è conveniente in termini di tempo.
  *      3 Devo controllare di aver messo bene i return, perchè devo poter uscire dai for appena una condizione non è soddisfatta.
+ *      4 Qua scansiono l'array di entità per vedere se trovo l' entità, ma dovrei controllare prima che ci sia, perchè se ancora non è inizializzato sarà null
+ *        La condizione del for potrebbe salvarmi, perchè se entNumber è zero, esco subito dal for e sto a posto.
  *
  *
  *
